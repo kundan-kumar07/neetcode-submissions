1e5bc4a09942c981769e097b35049fbc4f878ca3
Kundan kumar dubey <dubeykundan351@gmail.com>
2026-08-07 23:09:43 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int solve(int idx, vector<int>& coins, int amount,
+              vector<vector<int>>& dp)
+    {
+        if(amount == 0)
+            return 0;
+
+        if(amount < 0 || idx == coins.size())
+            return 100001;
+
+        // Already solved?
+        if(dp[idx][amount] != -1)
+            return dp[idx][amount];
+
+        int take = 100001;
+
+        int res = solve(idx, coins, amount - coins[idx], dp);
+
+        if(res != 100001)
+            take = 1 + res;
+
+        int notTake = solve(idx + 1, coins, amount, dp);
+
+        return dp[idx][amount] = min(take, notTake);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+
+        vector<vector<int>> dp(
+            coins.size(),
+            vector<int>(amount + 1, -1)
+        );
+
+        int ans = solve(0, coins, amount, dp);
+
+        return ans == 100001 ? -1 : ans;
+    }
+};
