cbb62569f100198e14b356608d8477a6d2873fcb
Kundan kumar dubey <dubeykundan351@gmail.com>
2026-08-15 09:48:11 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(int val:nums){
+            ans.push_back(val);
+        }
+        for(int val:nums){
+            ans.push_back(val);
+        }
+        return ans;
+        
+    }
+};
